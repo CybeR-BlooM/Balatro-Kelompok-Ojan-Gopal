273d6f0804e6e361567ea73d717a25d1a1bcd03f
Naufal <mnaufalaqil01@gmail.com>
2026-05-04 16:13:35 +0700

    Refactor PairChecker.cpp

--- Project-Balatro-Sampai-End/Project-Balatro-Sampai-End/PairChecker.cpp
+++ Project-Balatro-Sampai-End/Project-Balatro-Sampai-End/PairChecker.cpp
@@ -1,12 +1,34 @@
 #include "PairChecker.h"
 #include <iostream>
+#include <map> // Wajib di-include untuk menggunakan Tabel Frekuensi
 
 HandRank PairChecker::check(const Hand& hand) {
     std::cout << "-> Mengecek apakah kartu ini Pair...\n";
-    if (hand.forcedRank == HandRank::Pair) {
-        std::cout << "   [!] MATCH! Kombinasi Pair ditemukan!\n";
-        return HandRank::Pair;
+
+    if (hand.cards.size() == 5) {
+        // 1. Membuat Tabel Frekuensi
+        std::map<int, int> rankCounts;
+        for (const Card& c : hand.cards) {
+            rankCounts[c.rank]++;
+        }
+
+        // 2. Mengecek isi Tabel Frekuensi
+        bool isPair = false;
+        // Format [rank, count] adalah fitur C++ modern untuk membaca map dengan mudah
+        for (auto const& [rank, count] : rankCounts) {
+            if (count == 2) {
+                isPair = true;
+                break; // Ketemu 1 Pair, langsung berhenti mencari
+            }
+        }
+
+        if (isPair) {
+            std::cout << "   [!] MATCH! Kombinasi Pair ditemukan!\n";
+            return HandRank::Pair;
+        }
     }
+
+    // Jika gagal, lempar ke rantai selanjutnya (High Card)
     if (nextChecker) return nextChecker->check(hand);
     return HandRank::None;
 }
