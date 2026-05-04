10f6da52063a8cae81948503e278d1a4b7ca9415
Naufal <mnaufalaqil01@gmail.com>
2026-05-04 16:16:07 +0700

    Refactor ThreeOfAKindChecker.cpp

--- Project-Balatro-Sampai-End/Project-Balatro-Sampai-End/ThreeOfAKindChecker.cpp
+++ Project-Balatro-Sampai-End/Project-Balatro-Sampai-End/ThreeOfAKindChecker.cpp
@@ -1,12 +1,30 @@
 #include "ThreeOfAKindChecker.h"
 #include <iostream>
+#include <map>
 
 HandRank ThreeOfAKindChecker::check(const Hand& hand) {
     std::cout << "-> Mengecek apakah kartu ini Three of a Kind...\n";
-    if (hand.forcedRank == HandRank::ThreeOfAKind) {
-        std::cout << "   [!] MATCH! Kombinasi Three of a Kind ditemukan!\n";
-        return HandRank::ThreeOfAKind;
+
+    if (hand.cards.size() == 5) {
+        std::map<int, int> rankCounts;
+        for (const Card& c : hand.cards) {
+            rankCounts[c.rank]++;
+        }
+
+        bool hasThree = false;
+        for (auto const& [rank, count] : rankCounts) {
+            if (count == 3) {
+                hasThree = true;
+                break;
+            }
+        }
+
+        if (hasThree) {
+            std::cout << "   [!] MATCH! Kombinasi Three of a Kind ditemukan!\n";
+            return HandRank::ThreeOfAKind;
+        }
     }
+
     if (nextChecker) return nextChecker->check(hand);
     return HandRank::None;
 }
